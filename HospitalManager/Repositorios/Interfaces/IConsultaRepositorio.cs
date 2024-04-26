6921e647d3edc9754a1c77c3b32092212424aca1
João Vitor Soares <jvfs12th@gmail.com>
2024-04-26 14:33:46 -0300

    build: Cria interface para consultas

---
+++ HospitalManager/Repositorios/Interfaces/IConsultaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using HospitalManager.Models;
+
+namespace HospitalManager.Repositorios.Interfaces
+{
+    public interface IConsultaRepositorio
+    {
+        Task<List<Consulta>> BuscarTodasConsultas();
+        // Buscar consultas feitas, a fazer, canceladas etc..
+        Task<Consulta> BuscarPorId(Guid id);
+        Task<Consulta> Adicionar(Consulta consulta);
+        Task<Consulta> Atualizar(Consulta consulta);
+        Task<bool> Apagar(Guid id);
+    }
+}
