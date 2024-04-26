c05a5d33125718e6458bcca2b2a08718d4b3ed89
João Vitor Soares <jvfs12th@gmail.com>
2024-04-26 14:23:07 -0300

    feat: Adiciona informações da classe Consulta

--- HospitalManager/Models/Consulta.cs
+++ HospitalManager/Models/Consulta.cs
@@ -2,5 +2,14 @@
 {
     public class Consulta
     {
+        public Guid Id { get; set; }
+        public Guid MedicoId { get; set; }
+        public Medico Medico { get; set; }
+        public Guid PacienteId { get; set; }
+        public Paciente Paciente { get; set; }
+        public DateTime Data { get; set; }
+        public string MotivoConsulta { get; set; }
+        public int StatusProcedimento { get; set; }
+
     }
 }
